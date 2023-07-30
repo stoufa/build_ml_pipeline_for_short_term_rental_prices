7e210cb81ef341db6819fd7fb8e899987fb532a0
Mustapha Sahli <m.sahli@instadeep.com>
2023-07-30 09:56:24 +0100

    ADD: Visual Explanation for the Geolocation Fix
    to further explain the geolocation fix we did between versions 1.0.0 and 1.0.1

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -12,7 +12,6 @@ logger = logging.getLogger()
 
 
 def go(args):
-
     run = wandb.init(job_type="basic_cleaning")
     run.config.update(args)
 
@@ -24,28 +23,28 @@ def go(args):
     # YOUR CODE HERE     #
     ######################
 
-    logger.info('Downloading the data from W&B ...')
+    logger.info("Downloading the data from W&B ...")
     local_path = wandb.use_artifact("sample.csv:latest").file()
     df = pd.read_csv(local_path)
 
-    logger.info('Cleaning raw data ...')
+    logger.info("Cleaning raw data ...")
     # Drop outliers
-    idx = df['price'].between(args.min_price, args.max_price)
+    idx = df["price"].between(args.min_price, args.max_price)
     df = df[idx].copy()
     # Convert last_review to datetime
-    df['last_review'] = pd.to_datetime(df['last_review'])
+    df["last_review"] = pd.to_datetime(df["last_review"])
 
     # fix of the "test_proper_boundaries" test
     # drop rows in the dataset that are not in the proper geolocation (i.e. drop records outside of the area of NYC)
-    idx = df['longitude'].between(-74.25, -73.50) & \
-        df['latitude'].between(40.5, 41.2)
+    # refer to images/geolocation-fix-visual-explanation for a visual explanation
+    idx = df["longitude"].between(-74.25, -73.50) & df["latitude"].between(40.5, 41.2)
     df = df[idx].copy()
 
-    logger.info('Exporting clean data as CSV ...')
-    clean_data_file_name = 'clean_sample.csv'
+    logger.info("Exporting clean data as CSV ...")
+    clean_data_file_name = "clean_sample.csv"
     df.to_csv(clean_data_file_name, index=False)
 
-    logger.info('Uploading clean data to W&B ...')
+    logger.info("Uploading clean data to W&B ...")
     artifact = wandb.Artifact(
         args.output_artifact,
         type=args.output_type,
@@ -56,51 +55,41 @@ def go(args):
 
 
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser(description="A very basic data cleaning")
 
     # All parameters should be of type str except min_price and max_price that should be float
 
     parser.add_argument(
-        "--input_artifact",
-        type=str,
-        help="the input artifact",
-        required=True
+        "--input_artifact", type=str, help="the input artifact", required=True
     )
 
     parser.add_argument(
         "--output_artifact",
         type=str,
         help="the name for the output artifact",
-        required=True
+        required=True,
     )
 
     parser.add_argument(
         "--output_type",
         type=str,
         help="the type for the output artifact",
-        required=True
+        required=True,
     )
 
     parser.add_argument(
         "--output_description",
         type=str,
         help="a description for the output artifact",
-        required=True
+        required=True,
     )
 
     parser.add_argument(
-        "--min_price",
-        type=float,
-        help="the minimum price to consider",
-        required=True
+        "--min_price", type=float, help="the minimum price to consider", required=True
     )
 
     parser.add_argument(
-        "--max_price",
-        type=float,
-        help="the maximum price to consider",
-        required=True
+        "--max_price", type=float, help="the maximum price to consider", required=True
     )
 
     args = parser.parse_args()
